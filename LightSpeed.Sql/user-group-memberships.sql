30281ec89498807dbdda0f3bd0dc6315d557641d
Kyle Sullivan <kjsulls@gmail.com>
2017-05-11 09:05:43 -0500

    added packages

--- LightSpeed.Sql/user-group-memberships.sql
+++ LightSpeed.Sql/user-group-memberships.sql
@@ -1,28 +1,41 @@
+-- List of groups ‘User A’ has a member in
+select	distinct(g.id),
+		g.name
+from	groups g, users u, memberships m
+where	g.id = m.group_id
+and		u.id = m.[user_id]
+and		u.fName = 'User'
+and		u.lName = 'A'
+
+-- Users with memberships in either GroupA or GroupB
+select  distinct(u.id),
+		u.fName,
+		u.lName
+from	memberships m, groups g, users u
+where	m.group_id = g.id
+and		u.id = m.[user_id]
+and		(g.name = 'GroupA' or g.name = 'GroupB')
+
+
+-- Users with memberships in both GroupA and GroupB
 with results as
 (
-  select g.id as group_id,
-         g.name as group_name,
-         m.user_id as [user_id]
-  from     memberships m, groups g
-  where     m.group_id = g.id
-  and     m.group_id in (select g1.id from groups g1 where g1.name = 'GroupA' or g1.name = 'GroupB')
+	select  u.id,
+			u.fName,
+			u.lName
+	from	memberships m, groups g, users u
+	where	m.group_id = g.id
+	and		m.user_id = u.id
+	and		g.name = 'GroupA'
 
-  union
-
-  select g.id as group_id,
-         g.name as group_name,
-         m.user_id as [user_id]
-  from     memberships m, groups g
-  where     m.group_id = g.id
-  and     m.group_id in (select g1.id from groups g1 where g1.name = 'GroupA' and g1.name = 'GroupB')
+union
+	select  u.id,
+			u.fName,
+			u.lName
+	from	memberships m, groups g, users u
+	where	m.group_id = g.id
+	and		m.user_id = u.id
+	and		g.name = 'GroupB'
  
 )
-select    r.group_id,
-        r.group_name,
-        u.id as [user_id],
-        u.fName,
-        u.lName
-from    results r, users u
-where    r.[user_id] = u.id
-and        u.fName = 'User'
-and        u.lName = 'A'
+select * from results
